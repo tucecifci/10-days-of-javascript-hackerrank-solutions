5ef7efc9c42a0bdd2dcf66f8b22b3fc090517676
tucecifci <tucecifcii@gmail.com>
2024-12-30 09:52:05 +0300

    day 8 create a button added

---
+++ Day 8/Create a Button/script.js
@@ -0,0 +1,16 @@
+// const btn = document.getElementById("btn");
+// btn.innerHTML= 0;
+
+// btn.addEventListener("click", ()=> {
+//     let currentValue = parseInt(btn.innerHTML);
+//     btn.innerHTML = currentValue + 1;
+// })
+
+
+const btn = document.getElementById("btn");
+btn.innerHTML = 0;
+
+btn.addEventListener("click", ()=> {
+let currentValue =Number(btn.innerHTML);
+btn.innerHTML = currentValue + 1;
+})
